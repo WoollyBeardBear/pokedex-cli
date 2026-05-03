904883c8ce23daf2b162958883fc057712b9b571
WoollyBeardBear <peterakluth@gmail.com>
2026-05-03 15:54:47 -0600

    added explore

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*Config) error
+	callback    func(*Config, []string) error
 }
 
 type Locations struct {
@@ -27,6 +27,14 @@ type Locations struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"results"`
+}
+
+type LocationArea struct {
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
 }
 
 type Config struct {
@@ -56,12 +64,17 @@ var commands = map[string]cliCommand{
 		description: "page back through locations",
 		callback:    commandMapBack,
 	},
+	"explore": {
+		name:        "explore",
+		description: "find pokemon in each area",
+		callback:    commandExplore,
+	},
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var mapConfig Config
-	url := "https://pokeapi.co/api/v2/location-area"
+	url := "https://pokeapi.co/api/v2/location-area/"
 	mapConfig.Next = &url
 	const baseTime = 5 * time.Millisecond
 	mapConfig.Cache = pokecache.NewCache(baseTime)
@@ -74,8 +87,8 @@ func main() {
 		if val, ok := commands[cmd]; !ok {
 			fmt.Print("Unknown command\n")
 		} else {
-			fmt.Printf("Your command was: %s \n", cmd)
-			val.callback(&mapConfig)
+			//fmt.Printf("Your command was: %s %s\n", cmd, input[1:])
+			val.callback(&mapConfig, input[1:])
 		}
 
 	}
@@ -86,7 +99,7 @@ func cleanInput(text string) []string {
 	return lowerFields
 }
 
-func commandMap(c *Config) error {
+func commandMap(c *Config, args []string) error {
 	if c.Next == nil {
 		fmt.Println("you're on the last page")
 		return nil
@@ -99,7 +112,6 @@ func commandMap(c *Config) error {
 			return err
 		}
 		defer res.Body.Close()
-		fmt.Println("right after response")
 		body, err := io.ReadAll(res.Body)
 		if err != nil {
 			fmt.Println("error reading body")
@@ -126,15 +138,13 @@ func commandMap(c *Config) error {
 	}
 	c.Next = locations.Next
 	c.Previous = locations.Previous
-
 	for _, location := range locations.Results {
 		fmt.Printf("%s\n", location.Name)
 	}
 	return nil
-
 }
 
-func commandMapBack(c *Config) error {
+func commandMapBack(c *Config, args []string) error {
 	if c.Previous == nil {
 		fmt.Println("you're on the first page")
 		return nil
@@ -179,13 +189,50 @@ func commandMapBack(c *Config) error {
 	return nil
 }
 
-func commandExit(c *Config) error {
+func commandExit(c *Config, args []string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return fmt.Errorf("Did not exit program")
 }
 
-func commandHelp(c *Config) error {
+func commandHelp(c *Config, args []string) error {
 	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex\nmap: Displays a list of locations in the pokedex\nmapb: Displays the previous list of locations")
+	return nil
+}
+
+func commandExplore(c *Config, args []string) error {
+	if len(args) == 0 {
+		fmt.Println("Please include a location area after the explore command")
+		return nil
+	}
+
+	fmt.Printf("Exploring %s \n", args[0])
+	url := "https://pokeapi.co/api/v2/location-area/" + args[0]
+	body, ok := c.Cache.Get(url)
+	if !ok {
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Println("Error getting response")
+			return err
+		}
+
+		defer res.Body.Close()
+		body, err = io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("error reading body")
+			return err
+		}
+		c.Cache.Add(url, body)
+	}
+	var locationArea LocationArea
+	if err := json.Unmarshal(body, &locationArea); err != nil {
+		fmt.Printf("error unmarshalling data: %v\n", err)
+		return err
+	}
+	fmt.Println("Found Pokemon:")
+	for _, encounter := range locationArea.PokemonEncounters {
+		fmt.Printf("- %s \n", encounter.Pokemon.Name)
+	}
+
 	return nil
 }
